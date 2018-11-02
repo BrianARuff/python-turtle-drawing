8af7f147614e9c5120cb7146c8e8280b7b363cca
BrianARuff <thelonewolf198927@gmail.com>
2018-11-02 17:20:13 -0400

    if-elif-else with Python

---
+++ if-elif.py
@@ -0,0 +1,18 @@
+import turtle
+
+# Demonstrate if-elif-else with Turtle drawing Library
+
+print("Draw Sqaure or Triangle?")
+
+polygon = input()
+
+if polygon == "Triangle":
+    for i in range(0, 3):
+        turtle.forward(100)
+        turtle.left(120)
+elif polygon == "Square":
+    for i in range(0, 4):
+        turtle.forward(100)
+        turtle.right(90)
+else:
+    print("Invalid option")
