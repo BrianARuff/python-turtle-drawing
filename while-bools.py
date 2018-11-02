fbbae0ac19f3f1dd075b2c323958ba916519c0d5
BrianARuff <thelonewolf198927@gmail.com>
2018-11-02 18:29:54 -0400

    while loops with booleans

---
+++ while-bools.py
@@ -0,0 +1,24 @@
+import turtle
+
+print("S or T")
+polygon = input()
+
+print("How many?")
+count = input()
+count = int(count)
+if not count:
+    count = 1
+
+while count > 0:
+    if polygon == "T":
+        for i in range(0, 3):
+            turtle.forward(100)
+            turtle.left(120)
+    elif polygon == "S":
+        for i in range(0, 4):
+            turtle.forward(100)
+            turtle.right(90)
+    else:
+        print("Invalid Choice")
+    count -= 1
+    turtle.forward(10)
