2ffa21f1d7c602274323af3c1e34ed5a1ed2b225
BrianARuff <thelonewolf198927@gmail.com>
2018-11-04 20:28:09 -0500

    basic dictionaries

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+# Dictionary - Unique keys with values in the form of pairs.
+
+d = {}
+
+print(d)
+
+print(type(d))
+
+d['banana'] = 'taste the fruit'
+
+print(d)
+
+d[4] = 'four'
+
+print(d)
+
+jane = {
+    'name': 'Jane',
+    'age': 29,
+    'residence': 'Charlotte, NC, USA'
+}
+
+print(jane)
